617f621fe3774d38fd3ffeec3f9c2397d6cc1696
Luis Felipe La Rotta <larottao@gmail.com>
2022-07-22 16:06:07 -0500

    Bugfix: files on topmost folder were not displayed

--- LaRottaO.CSharp.IOUtilities/GetFilesInFolderAndSubfolders.cs
+++ LaRottaO.CSharp.IOUtilities/GetFilesInFolderAndSubfolders.cs
@@ -9,21 +9,24 @@ namespace LaRottaO.CSharp.IOUtilities
 {
     public class GetFilesInFolderAndSubfolders
     {
-        public Task<List<String>> getFilesInFolderAndSubfolders(String folderPath, String searchedExtension)
+        public Task<List<String>> getFilesInFolderAndSubfolders(String argRootFolderPath, String argSearchedExtension)
         {
             return Task.Run(() =>
             {
-                DirectoryInfo dirInfoCarpetaPadre = new DirectoryInfo(folderPath);
-                DirectoryInfo[] dirInfoSubCarpetas = dirInfoCarpetaPadre.GetDirectories();
-
+                DirectoryInfo directoryInfoRoot = new DirectoryInfo(argRootFolderPath);
+                DirectoryInfo[] directoryInfoSubfolders = directoryInfoRoot.GetDirectories();
                 List<String> foundFilesList = new List<String>();
 
-                foreach (DirectoryInfo dirInfoSubCarpeta in dirInfoSubCarpetas)
+                FileInfo[] fileInfoArray = directoryInfoRoot.GetFiles(argSearchedExtension);
+
+                foreach (FileInfo fileInfo in fileInfoArray)
+
                 {
-                    DirectoryInfo directoryInfo = new DirectoryInfo(dirInfoSubCarpeta.FullName);
+                    foundFilesList.Add(fileInfo.FullName);
+                }
 
-                    FileInfo[] fileInfoArray = directoryInfo.GetFiles(searchedExtension);
-
+                foreach (DirectoryInfo directoryInfoSubfolder in directoryInfoSubfolders)
+                {
                     foreach (FileInfo file in fileInfoArray)
 
                     {
